f901aeafaeda92309c04771a6621e6a6e44ad4b0
Moe-Salim91156 <hmodaisleem88@gmail.com>
2026-01-28 20:20:20 +0300

    added the auth service

--- internal/models/session.go
+++ internal/models/session.go
@@ -5,5 +5,5 @@ import "time"
 type Session struct {
 	SessionID string
 	UserID    string
-	CreatedAt time.Time
+	ExpiresAt time.Time
 }
--- internal/services/auth.go
+++ internal/services/auth.go
@@ -9,4 +9,65 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"time"
 )
-type 
+
+type AuthService struct {
+	userStore    store.UserStore
+	sessionStore store.SessionStore
+}
+
+func newAuthService(us store.UserStore, ss store.SessionStore) *AuthService {
+	return &AuthService{
+		userStore:    us,
+		sessionStore: ss,
+	}
+}
+
+func generateID() string {
+	bytes := make([]byte, 16)
+	rand.Read(bytes)
+	return hex.EncodeToString(bytes)
+}
+
+func (a *AuthService) Register(username, password string) error {
+	// hashiing the password using bcrypt , dont know how does it work really but we'll get to that
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	// create / fill
+	user := models.User{
+		ID:           generateID(),
+		Username:     username,
+		PasswordHash: string(hash),
+		CreatedAt:    time.Now(),
+	}
+	//creating a user
+	return a.userStore.Create(user)
+}
+
+func (a *AuthService) Login(username, password string) (string, error) {
+
+	user, err := a.userStore.GetByUsername(username)
+	if err != nil {
+		return "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return "", err
+	}
+
+	sessoin := models.Session{
+		SessionID: generateID(),
+		UserID:    user.ID,
+		ExpiresAt: time.Now().Add(10 * time.Hour), // whatever the number
+	}
+	if err := a.sessionStore.Create(sessoin); err != nil {
+		return "", err
+	}
+	return sessoin.SessionID, nil
+}
+
+func (a *AuthService) Logout(sessionID string) error {
+	return a.sessionStore.Delete(sessionID)
+}
